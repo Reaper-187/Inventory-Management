6edf2ec0eabd8ffaf9048922f26f7abf23c28de5
Reaper-187 <Abdulkader_cheik@outlook.de>
2026-08-06 20:51:28 +0200

    install - axios & types for api calls - tanstack-query for api-handling - wrapped QueryClientProvider around RouterProvider in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,10 +4,15 @@ import { Toaster } from "./components/ui/toast";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Toaster />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
